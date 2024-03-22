b3edbad9946365de5e0b989e91140f02d9a62e75
Ilona <150517376+LunyaLp@users.noreply.github.com>
2024-03-22 13:42:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,50 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int i, j, temp;
+  for (i = 1; i < len; i++) {
+    j = i;
+    temp = arr[i];
+    while (j > 0 && temp < arr[j - 1]) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = temp;
+  }
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int perem = len - 1;
+  while (arr[perem] > value) {
+    perem--;
+  }
+  int count = 0;
+  for (int i = 0; i < perem; i++) {
+    for (int j = perem; j > i; j--) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        count++;
+      } else if (arr[i] + arr[j] < value) {
+        break;
+      }
+    }
+  }
+  return count;
 }
